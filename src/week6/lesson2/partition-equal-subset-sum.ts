f07332b04072afc56ba191f7ee42ad1bd2a3f96a
quguiyou <quguiyou1076@qq.com>
2021-08-14 21:42:22 +0800

    partition-equal-subset-sum

---
+++ src/week6/lesson2/partition-equal-subset-sum.ts
@@ -0,0 +1,21 @@
+function canPartition(nums: number[]): boolean {
+  const memo: boolean[][] = Array.from({ length: nums.length }, () => [])
+  const core = (index: number, sum: number): boolean => {
+    if (index < 0 || sum < 0) return false
+
+    if (memo[index][sum] !== undefined) return memo[index][sum]
+    if (sum === 0) return true
+
+    return (memo[index][sum] =
+      core(index - 1, sum) || core(index - 1, sum - nums[index]))
+  }
+
+  let sum = 0
+  for (let i = 0; i < nums.length; ++i) {
+    sum += nums[i]
+  }
+
+  if (sum % 2 !== 0) return false
+
+  return core(nums.length - 1, sum / 2)
+}
