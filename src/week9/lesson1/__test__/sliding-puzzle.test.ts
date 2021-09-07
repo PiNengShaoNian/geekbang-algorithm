0065744a8cc60369246c2c50781565a236a78b50
quguiyou <quguiyou1076@qq.com>
2021-09-07 22:01:26 +0800

    启发式搜索

--- src/week9/lesson1/__test__/sliding-puzzle.test.ts
+++ src/week9/lesson1/__test__/sliding-puzzle.test.ts
@@ -1,4 +1,4 @@
-import { slidingPuzzle } from '../sliding-puzzle'
+import { slidingPuzzle, slidingPuzzle1 } from '../sliding-puzzle'
 
 test('slidingPuzzle', () => {
   expect(
@@ -16,6 +16,26 @@ test('slidingPuzzle', () => {
 
   expect(
     slidingPuzzle([
+      [4, 1, 2],
+      [5, 0, 3],
+    ])
+  ).toBe(5)
+
+  expect(
+    slidingPuzzle1([
+      [1, 2, 3],
+      [4, 0, 5],
+    ])
+  ).toBe(1)
+  expect(
+    slidingPuzzle1([
+      [1, 2, 3],
+      [5, 4, 0],
+    ])
+  ).toBe(-1)
+
+  expect(
+    slidingPuzzle1([
       [4, 1, 2],
       [5, 0, 3],
     ])
--- src/week9/lesson1/sliding-puzzle.ts
+++ src/week9/lesson1/sliding-puzzle.ts
@@ -1,3 +1,5 @@
+import { Comparable, PriorityQueue } from '../../week4/lesson1/priority-queue'
+
 export function slidingPuzzle(board: number[][]): number {
   const distToBegin: Map<string, number> = new Map()
   const distToEnd: Map<string, number> = new Map()
@@ -89,6 +91,111 @@ export function slidingPuzzle(board: number[][]): number {
     res = expand(queueEnd, distToEnd, distToBegin)
 
     if (res !== -1) return res
+  }
+
+  return -1
+}
+
+// type CostAndState = [cast: number, state: string]
+
+class CostAndState implements Comparable<CostAndState> {
+  constructor(public cost: number, public state: string) {}
+
+  compareTo(that: CostAndState): number {
+    return this.cost - that.cost
+  }
+}
+
+//启发式搜索
+export function slidingPuzzle1(board: number[][]): number {
+  const distToBegin: Map<string, number> = new Map()
+  const queueBegin = new PriorityQueue<CostAndState>()
+  const boardToStr = (b: number[][]): string => {
+    let ans = ''
+
+    for (let i = 0; i < 2; ++i) {
+      for (let j = 0; j < 3; ++j) {
+        ans += b[i][j]
+      }
+    }
+    return ans
+  }
+  if (boardToStr(board) === '123450') return 0
+
+  const strToBoard = (str: string): number[][] => {
+    let ans: number[][] = [[], []]
+
+    for (let i = 0; i < 2; ++i) {
+      for (let j = 0; j < 3; ++j) {
+        ans[i][j] = +str[i * 3 + j]
+      }
+    }
+
+    return ans
+  }
+  const drow = [-1, 0, 1, 0]
+  const dcol = [0, 1, 0, -1]
+
+  const targetRow = [-1, 0, 0, 0, 1, 5]
+  const targetCol = [-1, 0, 1, 2, 0, 1]
+  const evaluate = (boardStr: string): number => {
+    //[[1,2,3]
+    // [4,5,0]]
+
+    let ans = 0
+    for (let i = 0; i < boardStr.length; ++i) {
+      const num = +boardStr[i]
+      if (num === 0) continue
+
+      ans += Math.abs(targetRow[num] - ((i / 3) >> 0))
+      ans += Math.abs(targetCol[num] - (i % 3))
+    }
+
+    return ans
+  }
+
+  queueBegin.push(new CostAndState(0, boardToStr(board)))
+  distToBegin.set(boardToStr(board), 0)
+
+  while (queueBegin.size()) {
+    const cur = queueBegin.pop()!
+    const depth = distToBegin.get(cur.state)!
+
+    let row = 0
+    let col = 0
+    const b = strToBoard(cur.state)
+    findZero: for (let i = 0; i < 2; ++i) {
+      for (let j = 0; j < 3; ++j) {
+        if (b[i][j] === 0) {
+          row = i
+          col = j
+          break findZero
+        }
+      }
+    }
+
+    for (let k = 0; k < 4; ++k) {
+      const curBoard = strToBoard(cur.state)
+
+      const newRow = row + drow[k]
+      const newCol = col + dcol[k]
+
+      if (newRow < 0 || newRow >= 2 || newCol < 0 || newCol >= 3) continue
+
+      const temp = curBoard[row][col]
+      curBoard[row][col] = curBoard[newRow][newCol]
+      curBoard[newRow][newCol] = temp
+      const newBoardStr = boardToStr(curBoard)
+
+      if (newBoardStr === '123450') return depth + 1
+
+      if (distToBegin.has(newBoardStr)) continue
+
+      distToBegin.set(newBoardStr, depth + 1)
+      queueBegin.push(
+        new CostAndState(depth + 1 + evaluate(newBoardStr), newBoardStr)
+      )
+    }
   }
 
   return -1
