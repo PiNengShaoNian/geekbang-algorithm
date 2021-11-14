2b2fbc62f0ea8b86ad8e7a226ccc2fa860902699
quguiyou <quguiyou1076@qq.com>
2021-11-14 20:05:33 +0800

    平方分割

--- src/assignment/range-sum-query-immutable.ts
+++ src/assignment/range-sum-query-immutable.ts
@@ -16,3 +16,49 @@ export class NumArray {
     return this.presum[right + 1] - this.presum[left]
   }
 }
+
+export class NumArray1 {
+  private blockSize: number = 0
+  private data: number[] = []
+  private blocks: number[] = []
+  constructor(nums: number[]) {
+    const blockSize = Math.floor(Math.sqrt(nums.length))
+    this.blockSize = blockSize
+    this.data = nums
+    this.blocks = Array.from<number>({
+      length: Math.ceil(nums.length / blockSize),
+    }).fill(0)
+    for (let i = 0; i < nums.length; ++i) {
+      this.blocks[Math.floor(i / blockSize)] += nums[i]
+    }
+  }
+
+  sumRange(left: number, right: number): number {
+    const { blockSize, blocks, data } = this
+    if (!data.length) return 0
+    const blockStart = Math.floor(left / blockSize)
+    const blockEnd = Math.floor(right / blockSize)
+
+    let ans = 0
+    if (blockStart === blockEnd) {
+      for (let i = left; i <= right; ++i) {
+        ans += data[i]
+      }
+      return ans
+    }
+
+    for (let i = left; i < blockSize * (blockStart + 1); ++i) {
+      ans += data[i]
+    }
+
+    for (let i = blockStart + 1; i < blockEnd; ++i) {
+      ans += blocks[i]
+    }
+
+    for (let i = blockEnd * blockSize; i <= right; ++i) {
+      ans += data[i]
+    }
+
+    return ans
+  }
+}
